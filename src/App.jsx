14ae96209a330aa933234869a62b8f9fb5114a91
sheikhzim2004-del <sheikhzim2004@gmail.com>
2026-03-15 16:40:26 +0600

    creat a component folder and creat a file

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Countries from './assets/components/countries/countries'
+
+function App() {
+
+  return (
+    <>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
